a1c45a7841c921041e84c9ff08038811c0e5f8ae
madinaadzhi <madinaadzhigeldieva2004@gmail.com>
2023-12-24 01:13:40 +0200

    add backup and migration sql scripts

--- src/main/resources/db/migration/v03__aggr_products.sql
+++ src/main/resources/db/migration/v03__aggr_products.sql
@@ -2,9 +2,14 @@ create table aggr_products
 (
     id          bigint auto_increment
         primary key,
-    category_id bigint        not null,
+    category_id bigint       not null,
     image_url   varchar(256) null,
-    name        varchar(512)  not null
+    name        varchar(256) not null,
+    unit varchar (256) not null
+
 );
 
-create index aggr_products_image_url on aggr_products(image_url);
+create index aggr_products_image_url on aggr_products (image_url);
+create index aggr_products_category_id on aggr_products (category_id);
+create index aggr_products_name on aggr_products (name);
+create index aggr_products_unit on aggr_products (unit);
--- src/main/resources/db/migration/v04__products.sql
+++ src/main/resources/db/migration/v04__products.sql
@@ -3,18 +3,26 @@ create table product
     id              bigint auto_increment
         primary key,
     category_id     bigint        not null,
-    external_id     varchar(1024) not null,
+    external_id     varchar(256) not null,
     image_url       varchar(256) null,
     is_available    bit           not null,
-    name            varchar(512)  not null,
+    name            varchar(256)  not null,
+    unit            varchar(128)  not null,
     price           double        not null,
     site_url        varchar(1024) not null,
-    aggr_product_id bigint        null,
-    market_id       bigint        null,
+    aggr_product_id bigint null,
+    market_id       bigint null,
     constraint FKee90sc62qjuybcexvgyjln2cd
         foreign key (aggr_product_id) references aggr_products (id),
     constraint FKfl54i2ot7gmvmee2ers94uuao
-        foreign key (market_id) references markets (id)
+        foreign key (market_id) references markets (id),
+    CONSTRAINT UKCategoryIdExternalIdNameUnit UNIQUE (category_id, external_id, name, unit)
+
 );
 
-create index products_image_url on product(image_url);
+create index products_image_url on product (image_url);
+create index product_aggr_product_id on product (aggr_product_id);
+create index product_market_id on product (market_id);
+create index product_name on product (name);
+create index product_unit on product (unit);
+
